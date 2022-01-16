3a8e1f31f28fcc93a72c5c0fa776a0163f9c4aba
Raphael Andrade <raphaelandrade@Raphaels-MacBook-Pro.local>
2022-01-16 00:04:28 -0800

    change form to KendoReact  library

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,187 +1,173 @@
 import React, { useState } from "react";
+import { Form, Field } from "@progress/kendo-react-form";
 import countries from "./countries";
-import './App.css';
+import Input from "./Components/Input"
+
+
+
+const DropDown = ({ label, value, valid, visited, options,
+  onChange, onBlur, onFocus, validationMessage, }) => {
+  const invalid = !valid && visited;
+  return (
+    <div onBlur={onBlur} onFocus={onFocus}>
+      <label>
+        { label }
+        <select
+          className={invalid ? "invalid" : ""}
+          value={value}
+          onChange={onChange}>
+          <option key=""></option>
+          {options.map(option => (
+            <option key={option}>{option}</option>
+          ))}
+        </select>
+      </label>
+      { invalid && 
+        (<div className="required">{validationMessage}</div>) }
+    </div>
+  )
+}
+
+
+const requiredValidator = (value) => {
+  return value ? "" : "This field is required";
+}
 
 export default function App() {
-  const [firstName, setFirstname] = useState(""); 
-  const [lastName, setlastName] = useState(""); 
-  const [country, setCountry] = useState(""); 
-  const [dataOfBirth, setBirth] = useState(""); 
-  const [maritalStatus, setMaritalStatus] = useState(""); 
-  const [holidayAllowance, setHolidayAllowance] = useState("");
-  const [socialNumber, setSocialNumber] = useState("");
-  const [numberOfChildren, setNumberOfChildren] = useState("");
-  const [workingHours, setWorkingHours] = useState("");
+
   const [isSpain, setIsSpain] = useState(false); 
   const [isGhana, setIsGhana] = useState(false); 
-  const [isBrazil, setIsBrazil] = useState(false); 
-  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  const [isBrazil, setIsBrazil] = useState(false);
 
+  const handleIsSpain = (data) => { 
+    let country = data.target.value;
+    switch(country) {
+      case "Spain":
+        setIsSpain(true);
+        setIsBrazil(false);
+        setIsGhana(false);
+        break;
+      case "Brazil":
+        setIsSpain(false);
+        setIsBrazil(true);
+        setIsGhana(false);
+        break;
+      case "Ghana":
+        setIsSpain(false);
+        setIsBrazil(false);
+        setIsGhana(true);
+        break;
+      default:
+        setIsSpain(false);
+        setIsBrazil(false);
+        setIsGhana(false);
+    }
+  }
 
-
-  const handleSubmit = (event) => {
+  const handleSubmit = (data, event) => {
     console.log(`
-      First Name: ${firstName}
-      Last Name: ${lastName}
-      Country: ${country}
-      Marital Status: ${maritalStatus}
-      Holiday Allowance: ${holidayAllowance}
-      Social Number: ${socialNumber}
-      Accepted Terms: ${acceptedTerms}
+      Country: ${data.country}
     `);
     
     event.preventDefault();
   }
 
-  const handleIsSpain = (country) => {
-    if(country === "Spain") {
-      setIsSpain(true);
-      setIsBrazil(false);
-      setIsGhana(false);
-    } else if(country === "Brazil") {
-      setIsSpain(false);
-      setIsBrazil(true);
-      setIsGhana(false);
-    } else if(country === "Ghana") {
-      setIsSpain(false);
-      setIsBrazil(false);
-      setIsGhana(true);
-    } else {
-      setIsSpain(false);
-      setIsBrazil(false);
-      setIsGhana(false);
-    }
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Create Account</h1>
-      <label>
-        Country:
-        <select
-          name="country"
-          value={country}
-          onChange={e => {
-            handleIsSpain(e.target.value);
-            setCountry(e.target.value)
-          } }
-          required>
-          <option key=""></option>
-          {countries.map(country => (
-            <option key={country}>{country}</option>
-          ))}
-        </select>
-      </label>
+    <Form
+      onSubmit={handleSubmit}
+      initialValues={{
+        email: "", password: "", country: "", acceptedTerms: false
+      }}
+      render={(formRenderProps) => (
+        <form onSubmit={formRenderProps.onSubmit}>
+          <h1>Add new employee</h1>
 
-      <label>
-        First Name:
-        <input
-          name="firstName"
-          type="text"
-          value={firstName}
-          onChange={e => setFirstname(e.target.value)}
-          required />
-      </label>
-      
-      <label>
-        Last Name:
-        <input
-          name="lastName"
-          type="text"
-          value={lastName}
-          onChange={e => setlastName(e.target.value)}
-          required />
-      </label>
+          <Field 
+            label="Country:"
+            name="country"
+            component={DropDown}
+            options={countries}
+            validator={requiredValidator} 
+            onChange={handleIsSpain}/>
 
-      <label>
-        Data Of Birth:
-        <input
-          name="dataOfBirth"
-          type="date"
-          value={dataOfBirth}
-          onChange={e => setBirth(e.target.value)}
-          required />
-      </label>
+          <Field
+            label="First Name:"
+            name="FistName"
+            fieldType="text"
+            component={Input}
+            validator={[requiredValidator]} />
 
-      { isSpain ? <><label>
-        Marital Of Birth:
-        <input
-          name="maritalBirth"
-          type="text"
-          value={maritalStatus}
-          onChange={e => setMaritalStatus(e.target.value)}
-          required />
-      </label><label>
-          Social insurance number :
-          <input
-            name="socialInsurance"
-            type="text"
-            value={socialNumber}
-            onChange={e => setSocialNumber(e.target.value)}
-            required />
-        </label></>:
-      null
-      }
+          <Field
+            label="Last Name:"
+            name="lastName"
+            fieldType="text"
+            component={Input}
+            validator={[requiredValidator]} />
 
-      {isGhana ? <><label>
-        Marital Of Birth:
-        <input
-          name="maritalBirth"
-          type="text"
-          value={maritalStatus}
-          onChange={e => setMaritalStatus(e.target.value)}
-          required />
-      </label><label>
-          Social insurance number :
-          <input
-            name="numberOfChildren"
-            type="number"
-            value={numberOfChildren}
-            onChange={e => setSocialNumber(e.target.value)}
-            required />
-        </label></>:
-      null
-      }
+          <Field
+            label="Data Of Birth:"
+            name="dataBirth"
+            fieldType="date"
+            component={Input}
+            validator={[requiredValidator]} />
+            {isSpain && (<Field
+            label=" Marital Of Birth:"
+            name="MaritalBirthSpain"
+            fieldType="text"
+            component={Input}
+            validator={[requiredValidator]} />)}
+            
+            {isSpain && (<Field
+            label="Social insurance number:"
+            name="SocialInsurance"
+            fieldType="text"
+            component={Input}
+            validator={[requiredValidator]} />)}
 
-      {isBrazil ? 
-        <label>
-          Working hours
-          <input
-            name="dataOfBirth"
-            type="number"
-            value={dataOfBirth}
-            onChange={e => setBirth(e.target.value)}
-            required />
-        </label> : null
-       }
+            {isGhana && ( <Field
+            label=" Marital Of Birth:"
+            name="MaritalBirthGhana"
+            fieldType="text"
+            component={Input}
+            validator={[requiredValidator]} />)}
 
-      <label>
-        Holiday Allowance:
-        <input
-          name="holidayAllowance"
-          type="number"
-          minLength={30}
-          value={holidayAllowance}
-          onChange={e => setHolidayAllowance(e.target.value)}
-          required />
-      </label>
-      {isSpain ? <small>Minimum holiday allowance is 30 days</small> : null}
-      {isBrazil ? <small>Maximum holiday allowance is 40 days</small> : null}
+            {isGhana && (<Field
+            label="Number Of Children:"
+            name="numberChildren"
+            fieldType="number"
+            component={Input}
+            validator={[requiredValidator]} />)}
 
-      <label>
-        <input
-          name="acceptedTerms"
-          type="checkbox"
-          onChange={e => setAcceptedTerms(e.target.value)}
-          required />
-        I accept the terms of service        
-      </label>
+           {isBrazil && (<Field
+            label="Working hours:"
+            name="workingHours"
+            fieldType="number"
+            component={Input}
+            validator={[requiredValidator]} />)}
 
-      <button>Submit</button>
-    </form>
+            {isBrazil && (<Field
+            label="Holiday Allowance"
+            name="holidayAllowance"
+            fieldType="number"
+            component={Input}
+            validator={[requiredValidator]} />)
+            }
+            {isSpain && (<Field
+            label="Holiday Allowance"
+            name="holidayAllowance"
+            fieldType="number"
+            component={Input}
+            validator={[requiredValidator]} />)
+            }
+            
+            {isSpain && (<small>Minimum holiday allowance is 30 days</small>)}
+            {isBrazil && (<small>Maximum holiday allowance is 40 days</small>)}
+          
+          <button disabled={!formRenderProps.allowSubmit}>
+            Submit
+          </button>
+        </form>
+      )}>
+    </Form>
   );
 }
-
-
-
-
---
+++ my-app/src/Components/Input.js
@@ -0,0 +1,22 @@
+ const Input = (fieldProps) => {
+    const {
+      fieldType, label, value, visited, touched, valid,
+      onChange, onBlur, onFocus, validationMessage, 
+    } = fieldProps;
+    const invalid = !valid && visited;
+    return (
+      <div onBlur={onBlur} onFocus={onFocus}>
+        <label>
+          { label }
+          <input
+            type={fieldType}
+            className={invalid ? "invalid" : ""}
+            value={value}
+            onChange={onChange} />
+        </label>
+        { invalid && 
+          (<div className="required">{validationMessage}</div>) }
+      </div>
+    );
+  };
+  export default Input;
